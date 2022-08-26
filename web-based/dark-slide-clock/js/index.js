de418410df1bb4f6f11c4509fc8d3ca903d5f186
ivanc2002 <54784937+ivanc2002@users.noreply.github.com>
2022-08-26 13:06:08 -0700

    add files

---
+++ web-based/dark-slide-clock/js/index.js
@@ -0,0 +1,34 @@
+var size = 86;
+var columns = Array.from(document.getElementsByClassName('column'));
+var d = void 0,
+    c = void 0;
+var classList = ['visible', 'close', 'far', 'far', 'distant', 'distant'];
+var use24HourClock = true;
+
+function padClock(p, n) {
+	return p + ('0' + n).slice(-2);
+}
+
+function getClock() {
+	d = new Date();
+	return [use24HourClock ? d.getHours() : d.getHours() % 12 || 12, d.getMinutes(), d.getSeconds()].reduce(padClock, '');
+}
+
+function getClass(n, i2) {
+	return classList.find(function (class_, classIndex) {
+		return Math.abs(n - i2) === classIndex;
+	}) || '';
+}
+
+var loop = setInterval(function () {
+	c = getClock();
+
+	columns.forEach(function (ele, i) {
+		var n = +c[i];
+		var offset = -n * size;
+		ele.style.transform = 'translateY(calc(50vh + ' + offset + 'px - ' + size / 2 + 'px))';
+		Array.from(ele.children).forEach(function (ele2, i2) {
+			ele2.className = 'num ' + getClass(n, i2);
+		});
+	});
+}, 200 + Math.E * 10);
